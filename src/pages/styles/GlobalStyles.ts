9bfae4b6231cb0217870a6d541fad6fc414bfa0b
kasilianaoliveira <kasilianaoliveira@gmail.com>
2022-08-16 16:49:39 -0300

    Add global style

---
+++ src/pages/styles/GlobalStyles.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #2B4162;
+    font-family: 'Roboto', sans-serif;
+    color: white;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;

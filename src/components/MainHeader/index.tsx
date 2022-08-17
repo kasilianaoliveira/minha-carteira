378766c3027bd3f5655ad89f6c91f14270d9ffe8
kasilianaoliveira <kasilianaoliveira@gmail.com>
2022-08-16 23:13:44 -0300

    Add Header

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useMemo } from "react";
+import emojis from "../../utils/emojis";
+import { Toggle } from "../Toggle";
+import { Container, Profile, Welcome, UserName } from "./style";
+
+export const MainHeader: FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+
+    return emojis[indice];
+  }, []);
+  return (
+    <Container>
+      <Toggle />
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Kasiliana</UserName>
+      </Profile>
+    </Container>
+  );
+};
